f13e0ba62427d6fc2c9317992e42b6f54fdc7f95
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-23 12:42:02 +0300

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex,color, temperament,alive) VALUES ("Mr. Chocolate",2,"M", "red","polite", 1);
+INSERT INTO bears (name, age, sex,color, temperament,alive) VALUES ("Rowdy",4,"M", "grey","polite", 1);
+INSERT INTO bears (name, age, sex,color, temperament,alive) VALUES ("Tabitha",2,"F", "white","aggressive", 1);
+INSERT INTO bears (name, age, sex,color, temperament,alive) VALUES ("Sergeant Brown",2,"M", "black","Strong", 1);
+INSERT INTO bears (name, age, sex,color, temperament,alive) VALUES ("Melissa",3,"F", "brown","polite", 1);
+INSERT INTO bears (name, age, sex,color, temperament,alive) VALUES ("Grinch",6,"M", "red","kind", 1);
+INSERT INTO bears (name, age, sex,color, temperament,alive) VALUES ("Wendy",3,"F", "white","polite", 1);
+INSERT INTO bears (name, age, sex,color, temperament,alive) VALUES (NULL,2,"M", "red","polite", 1);
